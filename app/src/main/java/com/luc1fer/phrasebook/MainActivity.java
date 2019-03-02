e0753b1d99ed68b1ad8fa322de634be1a632a4a5
ruslkochnev <ruslkochnev@gmail.com>
2019-03-02 13:32:57 +0300

    add .db support

--- app/src/main/java/com/luc1fer/phrasebook/MainActivity.java
+++ app/src/main/java/com/luc1fer/phrasebook/MainActivity.java
@@ -1,10 +1,14 @@
 package com.luc1fer.phrasebook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
+import com.luc1fer.phrasebook.PhraseDBHelper;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
@@ -30,6 +34,60 @@ public class MainActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
+        Button button = findViewById(R.id.buttonTrip);
+
 
     }
+
+    public void onTripButtonClick(View view) {
+        Intent intent = new Intent(MainActivity.this, inflate.class);
+        intent.putExtra("i", 2);
+        intent.putExtra("j", 95);
+        intent.putExtra("title", "Путешествия");
+        startActivity(intent);
+    }
+
+/*
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonTrip:
+                Intent intentTrip = new Intent(mainBook.this, trip.class);
+                startActivity(intentTrip);
+                break;
+            case R.id.buttonTrans:
+                Intent intentTrans = new Intent(mainBook.this, trans.class);
+                startActivity(intentTrans);
+                break;
+            case R.id.buttonHotel:
+                Intent intentHotel = new Intent(mainBook.this, hotel.class);
+                startActivity(intentHotel);
+                break;
+            case R.id.buttonCafe:
+                Intent intentCafe = new Intent(mainBook.this, cafe.class);
+                startActivity(intentCafe);
+                break;
+            case R.id.buttonExc:
+                Intent intentExcursion = new Intent(mainBook.this, excursion.class);
+                startActivity(intentExcursion);
+                break;
+            case R.id.buttonBuy:
+                Intent intentBuy = new Intent(mainBook.this, buy.class);
+                startActivity(intentBuy);
+                break;
+            case R.id.buttonRadio:
+                Intent intentRadio = new Intent(mainBook.this, radio.class);
+                startActivity(intentRadio);
+                break;
+            case R.id.buttonBank:
+                Intent intentBank = new Intent(mainBook.this, bank.class);
+                startActivity(intentBank);
+                break;
+            case R.id.buttonHealth:
+                Intent intentHealth = new Intent(mainBook.this, health.class);
+                startActivity(intentHealth);
+                break;
+            case R.id.buttonCommon:
+                Intent intentCommon = new Intent(mainBook.this, common.class);
+                startActivity(intentCommon);
+                break;*/
 }
---
+++ app/src/main/java/com/luc1fer/phrasebook/inflate.java
@@ -0,0 +1,75 @@
+package com.luc1fer.phrasebook;
+
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+public class inflate extends AppCompatActivity {
+    private PhraseDBHelper mDBhelper;
+    private SQLiteDatabase mDb;
+    int i;
+    int j;
+    String title;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+        i = getIntent().getExtras().getInt("i");
+        j = getIntent().getExtras().getInt("j");
+        title = getIntent().getExtras().getString("title");
+        mDBhelper = new PhraseDBHelper(this);
+        try {
+            mDBhelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+        try {
+            mDb = mDBhelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
+        ArrayList<HashMap<String, Object>> commons = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> common;
+        Cursor cursor = mDb.rawQuery("SELECT * FROM phrase WHERE _id BETWEEN " + i +" AND " + j +";", null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            common = new HashMap<String, Object>();
+            common.put("russian",  cursor.getString(1));
+            common.put("english",  cursor.getString(2));
+            commons.add(common);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        String[] from = { "russian", "english"};
+        int[] to = {R.id.textViewRussian, R.id.textViewEnglish};
+        SimpleAdapter adapter = new SimpleAdapter(this, commons, R.layout.adapter_item, from, to);
+        ListView listView = findViewById(R.id.listViewItem);
+        listView.setAdapter(adapter);
+
+        Toolbar toolbar2 = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar2);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar2.setTitle(title);
+        toolbar2.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(inflate.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
