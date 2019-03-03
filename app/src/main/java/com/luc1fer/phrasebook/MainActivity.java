116898ffdd2f798cc8c3a96b01aa10b96e435160
ruslkochnev <ruslkochnev@gmail.com>
2019-03-03 15:46:39 +0300

    add .db support

--- app/src/main/java/com/luc1fer/phrasebook/MainActivity.java
+++ app/src/main/java/com/luc1fer/phrasebook/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-import com.luc1fer.phrasebook.PhraseDBHelper;
+
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
